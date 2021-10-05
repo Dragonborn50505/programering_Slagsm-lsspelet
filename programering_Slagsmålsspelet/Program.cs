d0069588e3cbbe87ae4d7000fd3409fc39563f45
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-10-05 11:08:32 +0200

    first_draft_just_need_some_fine_toning

--- programering_Slagsmålsspelet/Program.cs
+++ programering_Slagsmålsspelet/Program.cs
@@ -1,3 +1,33 @@
 ﻿using System;
 
 
+Random generator = new Random();
+
+
+int hp_Isak = 200;
+int hp_Jonathan = 200;
+
+while (hp_Isak > 0 && hp_Jonathan > 0)
+{
+    int damage = 10 + generator.Next(10, 21);
+    Console.WriteLine("Jonathan hit Isak and dealt ", damage, "damage");
+    hp_Isak -= damage;
+    Console.ReadLine();
+    damage = 0;
+
+
+    damage = 10 + generator.Next(10, 21);
+    Console.WriteLine("Isak hit Jonathan and dealt ", damage, "damage");
+    hp_Jonathan -= damage;
+    Console.ReadLine();
+}
+
+if (hp_Isak < 1)
+{
+Console.WriteLine("Game! Isak KO Jonathan");
+}
+
+if (hp_Jonathan < 1)
+{
+Console.WriteLine("Game! Jonathan KO Isak");
+}

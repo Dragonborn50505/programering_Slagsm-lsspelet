d1506a92701bcde87b33490c257d0f9028e46087
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-10-06 10:33:42 +0200

    Nivå_1_klart

--- programering_Slagsmålsspelet/Program.cs
+++ programering_Slagsmålsspelet/Program.cs
@@ -4,30 +4,52 @@
 Random generator = new Random();
 
 
-int hp_Isak = 200;
-int hp_Jonathan = 200;
+int hp_Isak = 100;
+int hp_Jonathan = 100;
+
+Console.WriteLine(@"
+       _.---._    /\\
+    ./'       '--`\//
+  ./              o \
+ /./\  )______   \__ \
+./  / /\ \   | \ \  \ \
+   / /  \ \  | |\ \  \7
+");
 
 while (hp_Isak > 0 && hp_Jonathan > 0)
 {
-    int damage = 10 + generator.Next(10, 21);
-    Console.WriteLine("Jonathan hit Isak and dealt ", damage, "damage");
+    int damage = 10  + generator.Next(0, 21);
+    Console.WriteLine($"Jonathan hit Isak and dealt {damage} damage");
     hp_Isak -= damage;
     Console.ReadLine();
     damage = 0;
 
 
-    damage = 10 + generator.Next(10, 21);
-    Console.WriteLine("Isak hit Jonathan and dealt ", damage, "damage");
+    damage = 10 + generator.Next(0, 21);
+    Console.WriteLine($"Isak hit Jonathan and dealt {damage} damage");
     hp_Jonathan -= damage;
     Console.ReadLine();
 }
 
-if (hp_Isak < 1)
+
+if (hp_Isak <= 1)
 {
-Console.WriteLine("Game! Isak KO Jonathan");
+    Console.WriteLine("Game! Isak KO Jonathan");
+    Console.ReadLine();
+
 }
 
-if (hp_Jonathan < 1)
+if (hp_Jonathan <= 1)
 {
-Console.WriteLine("Game! Jonathan KO Isak");
+    Console.WriteLine("Game! Jonathan KO Isak");
+    Console.ReadLine();
+
 }
+
+if (hp_Isak <= 0 && hp_Jonathan <= 0)
+{
+    Console.WriteLine("Double KO, both fighters are down");
+    Console.ReadLine();
+}
+
+//string name
